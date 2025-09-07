6260f7a5f3a216e05ba3c42bafc39e1260e70a26
SangamNirala <niralas594@gmail.com>
2025-09-07 08:30:02 +0000

    auto-commit for 4f488a77-0d54-4317-abbc-3042e2a1a0ef

---
+++ backend/ai_engines/__init__.py
@@ -0,0 +1,16 @@
+"""
+AI Engines Package
+Advanced AI-powered brand creation engines
+"""
+
+from .emergent_strategy import EmergentStrategyEngine
+from .gemini_visual import GeminiVisualEngine
+from .consistency_manager import ConsistencyManager
+from .export_engine import ExportEngine
+
+__all__ = [
+    'EmergentStrategyEngine',
+    'GeminiVisualEngine', 
+    'ConsistencyManager',
+    'ExportEngine'
+]
